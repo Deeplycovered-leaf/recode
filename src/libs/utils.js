0d1612106d7f5702dd319d7bd418401c15961bc1
zione <deeplyleaf@gmail.com>
2024-02-17 21:12:56 +0800

    feat: call

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -1,6 +1,21 @@
+/* eslint-disable no-eval */
 const utilsModule = ((Function) => {
-  Function.prototype.myCall = function () {
-    console.log('my call :>> ')
+  Function.prototype.myCall = function (ctx) {
+    ctx = Object(ctx) || window
+    ctx.originFn = this
+
+    // 拿 myCall 第二个参数开始到结束的所有参数作为 test 的实参列表
+    const args = []
+    for (let i = 1, len = arguments.length; i < len; i++)
+      args.push(`arguments[${i}]`)
+
+    // args + 字符串 -> args 展开平铺到 fn 实参中
+    // ctx.originFn(arguments[1],arguments[2])
+    const res = eval(`ctx.originFn(${args})`)
+
+    delete ctx.originFn
+
+    return res
   }
 })(Function)
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
+/* eslint-disable prefer-rest-params */
 /* eslint-disable no-unused-vars */
 import utilsModule from './libs/utils'
 
+/**
+ * 1. test -> call() -> test()
+ * 2. test -> this -> call 的第一个参数
+ * 3. call -> 第二个参数开始 -> test 的参数列表
+ */
 function test() {
+  console.log(this, arguments)
 
+  return 'over'
 }
 
-test.myCall()
+test.myCall({
+  a: 1,
+  b: 2,
+}, 'test1', 'test2')
+
+const obj = {
+  a: 1,
+  b: 2,
+  test() {
+    console.log(this)
+  },
+}
+
+// 对于一个方法来说，谁调用，函数内部的 this 就是指向谁
+obj.test()
